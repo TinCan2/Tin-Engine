846ffd7eb5dcda3c8efa88ad499a63a6e2e9846a
Can Kadıoğlu <cannkadioglu@gmail.com>
2024-02-17 16:14:31 +0100

    Added a .gitignore file and the GameManager class.

---
+++ Source/GameManager.cpp
@@ -0,0 +1,89 @@
+#include "GameManager.h"
+#include <SDL2/SDL.h>
+
+//Singleton Implementation
+GameManager* GameManager::Instantiate(){
+	if (currentManager != nullptr) return nullptr;
+	currentManager = new GameManager();
+	return currentManager;
+}
+
+GameManager* GameManager::GetCurrentInstance(){
+	return currentManager;
+}
+
+void GameManager::Destroy(){
+	if (currentManager != nullptr) delete currentManager;
+}
+
+
+//Game Events
+void GameManager::Initialize(const char* title, UInt16 w, UInt16 h) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	this->gameWindow = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_SHOWN);
+	this->mainRenderer = SDL_CreateRenderer(this->gameWindow, -1, SDL_RENDERER_PRESENTVSYNC |  SDL_RENDERER_ACCELERATED);
+
+//	Sprite::SetBoundedRenderer(this->mainRenderer);
+//	Camera::Instantiate(w, h);
+}
+
+void GameManager::Handle() {
+	SDL_RenderClear(this->mainRenderer);
+
+	SDL_Event currentEvent;
+	while (SDL_PollEvent(&currentEvent)) {
+		switch (currentEvent.type){
+			case SDL_QUIT:
+				this->QuitGame(); //This is temporary
+				break;
+		}
+	}
+}
+
+void GameManager::Update() {
+//	FunctionalObject::UpdateObjects();
+}
+
+void GameManager::Render() {
+//	VisualObject::RenderObjects();
+
+	SDL_RenderPresent(this->mainRenderer);
+	this->frameCount++;
+}
+
+void GameManager::Terminate() {
+//	Camera::Destroy();
+
+	SDL_DestroyRenderer(this->mainRenderer);
+	SDL_DestroyWindow(this->gameWindow);
+	SDL_Quit();
+}
+
+
+//Quitting
+bool GameManager::IsQuitting() {
+	return this->quitting;
+}
+
+void GameManager::QuitGame() {
+	this->quitting = true;
+}
+
+
+//Frames
+UInt64 GameManager::GetFrameCount() {
+	return this->frameCount;
+}
+
+
+//Construction and Destruction
+GameManager::GameManager() {
+	this->quitting = false;
+	this->frameCount = 0;
+}
+
+GameManager::~GameManager() {}
+
+
+//Statics
+GameManager* GameManager::currentManager;
