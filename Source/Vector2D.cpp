9ec37878f25b4f471af98bccd76cce4d461a96d6
Can Kadıoğlu <cannkadioglu@gmail.com>
2024-03-04 17:29:13 +0100

    Added the Vector2D class.

---
+++ Source/Vector2D.cpp
@@ -0,0 +1,86 @@
+#include "Vector2D.h"
+#include <cmath>
+
+//Construction and Destruction
+Vector2D::Vector2D() {
+	this->x = 0;
+	this->y = 0;
+}
+
+Vector2D::Vector2D(float x, float y) {
+	this->x = x;
+	this->y = y;
+}
+
+Vector2D::Vector2D(const Vector2D& coppiedVector) {
+	this->x = coppiedVector.x;
+	this->y = coppiedVector.y;
+}
+
+
+//Magnitude
+float Vector2D::GetMagnitude() {
+	return pow(this->x * this->x + this->y * this->y, 0.5);
+}
+
+
+//Operators
+Vector2D operator+(const Vector2D& a, const Vector2D& b) {
+	return Vector2D(a.x + b.x, a.y + b.y);
+}
+
+Vector2D operator+=(Vector2D& a, const Vector2D& b) {
+	a.x += b.x;
+	a.y += b.y;
+	return Vector2D(a);
+}
+
+Vector2D operator-(const Vector2D& a) {
+	return Vector2D(-a.x, -a.y);
+}
+
+Vector2D operator-(const Vector2D& a, const Vector2D& b) {
+	return Vector2D(a.x - b.x, a.y - b.y);
+}
+
+Vector2D operator-=(Vector2D& a, const Vector2D& b) {
+	a.x -= b.x;
+	a.y -= b.y;
+	return Vector2D(a);
+}
+
+Vector2D operator*(const Vector2D& a, const float& b) {
+	return Vector2D(a.x * b, a.y * b);
+}
+
+Vector2D operator*(const float& a, const Vector2D& b) {
+	return Vector2D(a * b.x, a * b.y);
+}
+
+float operator*(const Vector2D& a, const Vector2D& b) {
+	return a.x*b.x + a.y*b.y;
+}
+
+Vector2D operator*=(Vector2D& a, const float& b) {
+	a.x *= b;
+	a.y *= b;
+	return Vector2D(a);
+}
+
+Vector2D operator/(const Vector2D& a, const float& b) {
+	return Vector2D(a.x / b, a.y / b);
+}
+
+Vector2D operator/(const float& a, const Vector2D& b) {
+	return Vector2D(b.x / a, b.y / a);
+}
+
+Vector2D operator/=(Vector2D& a, const float& b) {
+	a.x /= b;
+	a.y /= b;
+	return Vector2D(a);
+}
+
+bool operator==(const Vector2D& a, const Vector2D& b) {
+	return (abs(a.x - b.x) < 0.0001f) && (abs(a.y - b.y) < 0.0001f);
+}
---
+++ Source/Vector2D.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+struct Vector2D {
+	float x, y;
+
+	Vector2D();
+	Vector2D(float x, float y);
+
+	Vector2D(const Vector2D& coppiedVector);
+
+	float GetMagnitude();
+
+	friend Vector2D operator+(const Vector2D& a, const Vector2D& b);
+
+	friend Vector2D operator+=(Vector2D& a, const Vector2D& b);
+
+	friend Vector2D operator-(const Vector2D& a);
+	friend Vector2D operator-(const Vector2D& a, const Vector2D& b);
+
+	friend Vector2D operator-=(Vector2D& a, const Vector2D& b);
+
+	friend Vector2D operator*(const Vector2D& a, const float& b);
+	friend Vector2D operator*(const float& a, const Vector2D& b);
+	friend float operator*(const Vector2D& a, const Vector2D& b);
+
+	friend Vector2D operator*=(Vector2D& a, const float& b);
+
+	friend Vector2D operator/(const Vector2D& a, const float& b);
+
+	friend Vector2D operator/=(Vector2D& a, const float& b);
+
+	friend bool operator==(const Vector2D& a, const Vector2D& b);
+
+	constexpr static int UnitPixelEquivalent = 16;
+};
