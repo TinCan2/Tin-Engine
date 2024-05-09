d3ced77abfeaba03d81c4ce938513f1f0b20da45
Can Kadıoğlu <cannkadioglu@gmail.com>
2024-05-09 15:51:11 +0200

    Updated the Vector2D structure.

--- Source/Vector2D.h
+++ Source/Vector2D.h
@@ -15,27 +15,27 @@ namespace Tin {
 		Vector2D FlipH();
 		Vector2D FlipV();
 
-		friend Vector2D operator+(const Vector2D& a, const Vector2D& b);
-
-		friend Vector2D operator+=(Vector2D& a, const Vector2D& b);
-
-		friend Vector2D operator-(const Vector2D& a);
-		friend Vector2D operator-(const Vector2D& a, const Vector2D& b);
-
-		friend Vector2D operator-=(Vector2D& a, const Vector2D& b);
-
-		friend Vector2D operator*(const Vector2D& a, const float& b);
-		friend Vector2D operator*(const float& a, const Vector2D& b);
-		friend float operator*(const Vector2D& a, const Vector2D& b);
-
-		friend Vector2D operator*=(Vector2D& a, const float& b);
-
-		friend Vector2D operator/(const Vector2D& a, const float& b);
-
-		friend Vector2D operator/=(Vector2D& a, const float& b);
-
-		friend bool operator==(const Vector2D& a, const Vector2D& b);
-
 		constexpr static UInt16 UnitPixelEquivalent = 16;
 	};
+
+	Vector2D operator+(const Vector2D& a, const Vector2D& b);
+
+	Vector2D operator+=(Vector2D& a, const Vector2D& b);
+
+	Vector2D operator-(const Vector2D& a);
+	Vector2D operator-(const Vector2D& a, const Vector2D& b);
+
+	Vector2D operator-=(Vector2D& a, const Vector2D& b);
+
+	Vector2D operator*(const Vector2D& a, const float& b);
+	Vector2D operator*(const float& a, const Vector2D& b);
+	float operator*(const Vector2D& a, const Vector2D& b);
+
+	Vector2D operator*=(Vector2D& a, const float& b);
+
+	Vector2D operator/(const Vector2D& a, const float& b);
+
+	Vector2D operator/=(Vector2D& a, const float& b);
+
+	bool operator==(const Vector2D& a, const Vector2D& b);
 }
