4c43310902a569b420aff0916cae68c75d1b91f5
Can Kadıoğlu <cannkadioglu@gmail.com>
2024-07-22 09:06:39 +0300

    Added the JointShape class.

---
+++ Source/JointShape.h
@@ -0,0 +1,41 @@
+#pragma once
+
+namespace Tin {
+	struct Vector2D;
+
+	class Circle;
+	class Rectangle;
+
+	typedef unsigned int UInt16;
+
+	class JointShape {
+		public:
+		JointShape(Circle* cSubs, UInt16 cCount, Rectangle* rSubs, UInt16 rCount, const Vector2D& center);
+		JointShape(const JointShape& coppiedObject);
+		~JointShape();
+
+		void SetCenter(const Vector2D& center);
+		Vector2D GetCenter() const;
+
+		void SetOrientation(const float& orientation);
+		float GetOrientation() const;
+
+		UInt16 GetCircleCount() const;
+		Circle GetCircle(UInt16 index) const;
+		UInt16 GetRectangleCount() const;
+		Rectangle GetRectangle(UInt16 index) const;
+
+		Circle GetEnclosure() const;
+
+		private:
+		Circle** circleSubs;
+		Rectangle** rectangleSubs;
+
+		UInt16 circleCount, rectangleCount;
+
+		Vector2D* center;
+		float orientation;
+
+		Circle* enclosure;
+	};
+}
