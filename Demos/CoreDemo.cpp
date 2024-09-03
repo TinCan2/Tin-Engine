91a7d2fee6fae45de47d080f98abfabdb392450f
Can Kadıoğlu <cannkadioglu@gmail.com>
2024-09-03 22:07:47 +0300

    Added the demo system.

---
+++ Demos/CoreDemo.cpp
@@ -0,0 +1,20 @@
+#include "GameManager.hpp"
+
+using namespace Tin;
+
+int main(int argc, char* argv[]) {
+	GameManager* gameManager = GameManager::Instantiate();
+
+	gameManager->Initialize("Input Demo", 640, 480);
+
+	while(!gameManager->IsQuitting()) {
+		gameManager->Handle();
+		gameManager->Update();
+		gameManager->Render();
+	}
+
+	gameManager->Terminate();
+
+	GameManager::Destroy();
+	return 0;
+}
