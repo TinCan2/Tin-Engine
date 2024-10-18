5a2c9d5b96aacb95d650485c7bfd233cfa62cca9
Can Kadıoğlu <cannkadioglu@gmail.com>
2024-10-19 00:00:42 +0200

    Cleaned up the Tilemap demo.

--- Demos/TilemapDemo.cpp
+++ Demos/TilemapDemo.cpp
@@ -1,74 +1,15 @@
-#include "Animation.hpp"
-#include "FunctionalObject.hpp"
+#include "Circle.hpp"
 #include "GameManager.hpp"
-#include "KeyboardManager.hpp"
 #include "Painter.hpp"
 #include "PhysicalObject.hpp"
 #include "Rectangle.hpp"
-#include "Sprite.hpp"
 #include "Tilemap.hpp"
 #include "Tileset.hpp"
 #include "Vector2D.hpp"
-#include "VisualObject.hpp"
 #include <cmath>
 #include <limits>
 
-#include <iostream>
-
 using namespace Tin;
-
-
-class Player : public FunctionalObject, public PhysicalObject, public VisualObject {
-	public:
-	Player() : PhysicalObject(Rectangle(Vector2D(0,7.0/4),Vector2D(3.0/4,7.0/4)),1), VisualObject(Vector2D(0,0),1) {
-		if (animIdle == nullptr || animWalk == nullptr) {
-			animIdle = new Animation(TIN_DEMO_LOCATION "Sprites/spr_player_default-Sheet.png", 24, 56, 45, Vector2D(3.0/4, 0));
-			animWalk = new Animation(TIN_DEMO_LOCATION "Sprites/spr_player_walk-Sheet.png", 26, 56, 10, Vector2D(3.0/4, 0));
-		}
-
-		this->SetVisual(*animIdle);
-		this->SetLockRotation(true);
-	}
-
-	virtual void OnUpdate() override {
-		this->SetVelocity(this->GetVelocity() + Vector2D(0,-5)*PhysicalObject::GetDeltaTime());
-
-		Painter p;
-		p.PaintRectangle(this->GetColliderAsRectangle());
-
-		KeyboardManager* keyboardManager = KeyboardManager::GetCurrentManager();
-
-		int direction = keyboardManager->KeyDown("right") - keyboardManager->KeyDown("left");
-
-		if (fabs(this->GetVelocity().x + this->acceleration*direction*GetDeltaTime()) <= this->maxSpeed) {
-			this->SetVelocity(this->GetVelocity() + Vector2D(this->acceleration*direction*GetDeltaTime(),0));
-		}
-		if (direction == 0) {
-			float sign = (this->GetVelocity().x == 0) ? 0 : this->GetVelocity().x/fabs(this->GetVelocity().x);
-			this->SetVelocity(this->GetVelocity() - sign*Vector2D(decceleration*GetDeltaTime(),0));
-
-			if (fabs(this->GetVelocity().x) <= decceleration*GetDeltaTime()) {
-				this->SetVelocity(Vector2D(0, this->GetVelocity().y));
-			}
-		}
-
-		if (direction != 0) this->SetFlip(direction < 0, false);
-		if (direction != 0) this->SetVisual(*animWalk);
-		else this->SetVisual(*animIdle);
-	}
-
-	private:
-	float maxSpeed = 7.5;
-	float acceleration = 20;
-	float decceleration = 20;
-
-	static Animation* animIdle;
-	static Animation* animWalk;
-};
-
-Animation* Player::animIdle = nullptr;
-Animation* Player::animWalk = nullptr;
-
 
 int main(int argc, char* argv[]) {
 	GameManager* gameManager = GameManager::Instantiate();
@@ -117,11 +58,14 @@ int main(int argc, char* argv[]) {
 
 	map.GeneratePhysics();
 
-	Player p;
+	PhysicalObject ball(Circle(Vector2D(13,0),2),1);
+	Painter p;
 
 	while(!gameManager->IsQuitting()) {
 		gameManager->Handle();
+		ball.SetVelocity(ball.GetVelocity() + Vector2D(0,-3)*PhysicalObject::GetDeltaTime());
 		gameManager->Update();
+		p.PaintCircle(ball.GetColliderAsCircle());
 		gameManager->Render();
 	}
 
